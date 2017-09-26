790ea26cfa26ffda7a066d8e7265d5a4b861b952
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2017-09-26 11:45:01 -0400

    Start Attendence Project

---
+++ GarrettChen/src/attendance/Student.java
@@ -0,0 +1,36 @@
+package attendance;
+
+public class Student implements Attendee {
+
+	public Student(String firstName, String lastName) {
+		
+	}
+
+	public boolean isPresent() {
+		
+	}
+	
+	public void setPresent(boolean present) {
+		
+	}
+	
+	public String getFirstName() {
+		
+	}
+	
+	public String getLastName() {
+		
+	}
+	
+	public boolean matches(String first, String last) {
+		
+	}
+	
+	public boolean matches(String last) {
+		
+	}
+	
+	public String getReportString() {
+		
+	}
+}
