ee1b14d8a7d794118d8b6ffc854ce2fc39aa4fa4
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2017-10-31 11:45:22 -0400

    a

---
+++ GarrettChen/src/caveExplorer/NPC.java
@@ -0,0 +1,29 @@
+package caveExplorer;
+
+/**
+ * NPC means non-playable character
+ * @author BT_1N3_31
+ *
+ */
+public class NPC {
+	
+	public NPC() {
+		
+	}
+
+	public boolean isActive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void interact() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getInactiveDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
