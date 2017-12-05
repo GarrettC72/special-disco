7cbafebe1ad5874ffee5fd5c8a5d893e1d88886a
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2017-12-05 10:41:02 -0500

    fibonacci

---
+++ GarrettChen/src/search/FibonacciNumber.java
@@ -0,0 +1,20 @@
+package search;
+
+public class FibonacciNumber {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 50; i++) {
+			System.out.println(i+ ". "+
+		fibonacci(i));
+		}
+	}
+	
+	public static int fibonacci(int n) {
+		if(n == 0 || n == 1) {
+			return 1;
+		}else {
+			return fibonacci(n - 1) + fibonacci(n - 2);
+		}
+	}
+
+}
